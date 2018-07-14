40d0165b502859de3ff71d2067ff8d2c4242d16a
ali aziz <aa_connect@outlook.com>
2018-07-14 20:49:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map { |attendee, index|  }
+  attendees.each_with_index.map do  |attendee, index| 
   "Hello, #{attendee}! you'll be assigned to room #{index+1}"
 end
 end
